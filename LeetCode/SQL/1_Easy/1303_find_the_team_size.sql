f5da981b7deee476d49a0ac91118bf28dbd85297
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-04-02 14:05:22 +0900

    :memo: [ LeetCode ] SQL Easy : 1303. Find the Team Size

--- LeetCode/SQL/1_Easy/1303_find_the_team_size.sql
+++ LeetCode/SQL/1_Easy/1303_find_the_team_size.sql
@@ -11,4 +11,13 @@ JOIN (
     FROM Employee
     GROUP BY team_id
 ) AS Teams
-USING (team_id)
+USING (team_id);
+
+
+-- 아래와 같이 WINDOW 함수를 사용하여 해결할 수도 있다.
+
+SELECT
+    employee_id,
+    COUNT(employee_id) OVER(PARTITION BY team_id) AS team_size
+FROM Employee
+ORDER BY team_size DESC, employee_id ASC;
