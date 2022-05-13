1a99ae2f23f288c39b2bdb5d32976988abba1b34
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-05-13 21:09:59 +0900

    :memo: [ LeetCode ] SQL Medium : 1393. Capital Gain/Loss

---
+++ LeetCode/SQL/2_Medium/1393_capital_gain_and_loss.sql
@@ -0,0 +1,7 @@
+-- [ LeetCode ] 1393. Capital Gain/Loss
+
+SELECT
+    stock_name,
+    SUM(CASE WHEN operation='Sell' THEN price ELSE -price END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
