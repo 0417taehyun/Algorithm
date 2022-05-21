d5f60b51cc291893b4208ee5383e5af69356e492
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-05-22 00:55:52 +0900

    :memo: [ LeetCode ] SQL Medium : 1715. Count Apples and Oranges

---
+++ LeetCode/SQL/2_Medium/1715_count_apples_and_oranges.sql
@@ -0,0 +1,8 @@
+-- [ LeetCode ] 1715. Count Apples and Oranges
+
+SELECT
+    SUM(Boxes.apple_count + IFNULL(Chests.apple_count, 0)) AS apple_count,
+    SUM(Boxes.orange_count + IFNULL(Chests.orange_count, 0)) AS orange_count
+FROM Boxes
+LEFT JOIN Chests
+USING (chest_id);
