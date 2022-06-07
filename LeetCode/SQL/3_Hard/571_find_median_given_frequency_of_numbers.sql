8b3e36ecdb820f4cbbaa8f4b6e7ba58899c71cd7
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-06-07 22:13:15 +0900

    :recycle: Add Description [ LeetCode ] SQL Hard : 571. Find Median Given Frequency of Numbers

--- LeetCode/SQL/3_Hard/571_find_median_given_frequency_of_numbers.sql
+++ LeetCode/SQL/3_Hard/571_find_median_given_frequency_of_numbers.sql
@@ -1,5 +1,13 @@
 -- [ LeetCode ] 571. Find Median Given Frequency of Numbers
 
+/*
+처음에는 아래와 같이 복잡하게 문제를 풀었다.
+WITH RECURSIVE 구에서 WHERE 구를 통해 제약을 걸어 전체 개수가 홀수인 경우와 짝수인 경우를 나눠서 중간값을 구했다.
+
+이때 유의할 점은 UNION ALL 키워드 다음에 오는 SELECT 구가 결국 WHERE 구보다 이전에 실행된다는 것이다. 
+추가적으로 SUM 윈도우 함수(Window Function)의 경우 OVER 부분에 아무런 조건을 서술하지 않으면 누적합이 아닌 전체 합을 구한다.
+*/
+
 WITH RECURSIVE MedianPositions (mid_number, flag) AS (
     SELECT
         ROUND(SUM(frequency) / 2) AS mid_number,
