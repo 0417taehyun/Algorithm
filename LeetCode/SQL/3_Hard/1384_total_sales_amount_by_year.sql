e4dfbe7582cc8d603fd0a0b00bc6f77c1da810e9
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-06-07 23:39:23 +0900

    :recycle: Add Different Solution [ LeetCode ] SQL Hard : 571. Find Median Given Frequency of Numbers

--- LeetCode/SQL/3_Hard/1384_total_sales_amount_by_year.sql
+++ LeetCode/SQL/3_Hard/1384_total_sales_amount_by_year.sql
@@ -30,45 +30,5 @@ GROUP BY Product.product_id, YEAR(YearsPoints.start_date)
 ORDER BY product_id ASC, report_year ASC;
 
 
-/*
-아래와 같이 수학적인 접근을 통해 문제를 해결할 수 있다.
-어떤 수 N의 이전 수들의 개수와 다음 수들의 개수가 같다면 해당 N은 중간에 있는 값이며 만약 다르다면 그 다른 범위를 N의 개수로 채울 수 있다.
-따라서 여기서 개수를 의미하는 frequency 필드를 활용하여 이전 수들의 개수와 다음 수들의 개수의 차이가 만약 현재 수의 개수보다 작거나 같다면 해당 수는 중간값이다.
-*/
-
-SELECT AVG(Numbers.num) AS median
-FROM Numbers
-WHERE Numbers.frequency >= ABS(
-    (SELECT SUM(Subnumbers.frequency) FROM Numbers AS Subnumbers WHERE Numbers.num >= Subnumbers.num)
-    -
-    (SELECT SUM(Subnumbers.frequency) FROM Numbers AS Subnumbers WHERE Numbers.num <= Subnumbers.num)
-);
 
 
--- 아래와 같이 WITH 구를 통해 위 방법을 조금 더 깔금하게 바꾸어 해결할 수 있다.
-
-WITH PreviousNumbers (num, previous_counts) AS (
-    SELECT
-        Numbers.num,
-        SUM(Subnumbers.frequency) AS previous_counts
-    FROM Numbers
-    JOIN Numbers AS Subnumbers
-    ON Numbers.num <= Subnumbers.num
-    GROUP BY Numbers.num
-), NextNumbers (num, next_counts) AS (
-    SELECT
-        Numbers.num,
-        SUM(Subnumbers.frequency) AS next_counts
-    FROM Numbers
-    JOIN Numbers AS Subnumbers
-    ON Numbers.num >= Subnumbers.num
-    GROUP BY Numbers.num
-)
-
-SELECT AVG(Numbers.num) AS median
-FROM Numbers
-JOIN PreviousNumbers
-USING (num)
-JOIN NextNumbers
-USING (num)
-WHERE Numbers.frequency >= ABS(PreviousNumbers.previous_counts - NextNumbers.next_counts);
