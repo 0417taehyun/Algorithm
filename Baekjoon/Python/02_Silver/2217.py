709f379e2efc7cb8f3e66531ed7f72ca78fe4c47
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2022-06-16 01:40:24 +0900

    :recycle: Fix to use the wallus operation [ 백준 ] Silver : 2217. 로프

--- Baekjoon/Python/02_Silver/2217.py
+++ Baekjoon/Python/02_Silver/2217.py
@@ -3,21 +3,24 @@
 def solution() -> None:
     N: int = int(input())
     ropes: list[int] = [ int(input()) for _ in range(N) ]
-    
-    answer = 0
+
     ropes.sort(reverse=True)
+    answer: int = ropes[0]
     for idx, weight in enumerate(ropes):
-        if weight * (idx + 1) > answer:
-            answer = weight
-            
+        if (current_weight := weight * (idx + 1)) > answer:
+            answer = current_weight
+
     print(answer)
-    
+        
 
 def short_solution() -> None:
     N: int = int(input())
     ropes: list[int] = [ int(input()) for _ in range(N) ]
     
-    print(max([ weight * (idx + 1) for idx, weight in enumerate(sorted(ropes)) ]))            
+    print(max([
+        weight * (idx + 1) for idx, weight
+        in enumerate(sorted(ropes, reverse=True))
+    ]))
 
 
 if __name__ == "__main__":
