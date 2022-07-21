4973771227b8e3bd03496149785e1ee2b7343037
0417taehyun <0417taehyun@gmail.com>
2022-07-21 23:17:40 +0900

    :recycle: Add Description [ LeetCode ] SQL Hard : 2153. The Number of Passengers in Each Bus II

--- LeetCode/SQL/3_Hard/2153_the_number_of_passengers_in_each_bus_II.sql
+++ LeetCode/SQL/3_Hard/2153_the_number_of_passengers_in_each_bus_II.sql
@@ -1,47 +1,50 @@
 -- [ LeetCode ] 2153. The Number of Passengers in Each Bus II
 
-WITH RECURSIVE cte1 (row_num, bus_id, capacity, current_passengers_count) AS (
+/*
+누적된 차이를 계속해서 비교해가야 하기 때문에 WITH RECURSIVE 공통 테이블 표현식을 활용해서 문제를 풀 수 있다.
+이때 arrival_time 필드를 기준으로 오름차순 정렬한 ROW_NUMBER 윈도우 함수(Window Function)를 사용해서 재귀의 기준이 되는 JOIN 구의 조건을 성사시킬 수 있다.
+*/
+
+WITH RECURSIVE cte1 (row_num, bus_id, capacity, waited_passengers_cnt) AS (
     SELECT
         ROW_NUMBER() OVER(ORDER BY Periods.arrival_time ASC) AS row_num,
-        bus_id,
-        capacity,
-        COUNT(Passengers.passenger_id) AS current_passengers_count
+        Periods.bus_id,
+        Periods.capacity,
+        COUNT(Passengers.passenger_id) AS waited_passengers_cnt
     FROM (
         SELECT
             bus_id,
-            LAG(arrival_time, 1, -1) OVER(ORDER BY arrival_time ASC) AS previous_time,
+            capacity,            
             arrival_time,
-            capacity
+            LAG(arrival_time, 1, -1) OVER(ORDER BY arrival_time ASC) AS previous_time
         FROM Buses
     ) AS Periods
     LEFT JOIN Passengers
     ON (
-        Periods.previous_time < Passengers.arrival_time
-        AND
         Periods.arrival_time >= Passengers.arrival_time
+        AND
+        Periods.previous_time < Passengers.arrival_time
     )
     GROUP BY Periods.bus_id
 ), cte2 (row_num, bus_id, diff, passengers_cnt) AS (
     SELECT
         row_num,
         bus_id,
-        IF(capacity < current_passengers_count, current_passengers_count - capacity, 0) AS diff,
-        IF(capacity < current_passengers_count, capacity, current_passengers_count) AS passengers_cnt
+        IF(capacity < waited_passengers_cnt, waited_passengers_cnt - capacity, 0) AS diff,
+        IF(capacity < waited_passengers_cnt, capacity, waited_passengers_cnt) AS passengers_cnt
     FROM cte1
     WHERE row_num = 1
     UNION ALL
     SELECT
         cte1.row_num,
         cte1.bus_id,
-        IF(cte1.capacity < cte1.current_passengers_count + cte2.diff, cte1.current_passengers_count + cte2.diff - cte1.capacity, 0) AS diff,
-        IF(cte1.capacity < cte1.current_passengers_count + cte2.diff, cte1.capacity, cte1.current_passengers_count + cte2.diff) AS passengers_cnt
+        IF(cte1.capacity < cte1.waited_passengers_cnt + cte2.diff, cte1.waited_passengers_cnt + cte2.diff - cte1.capacity, 0) AS diff,
+        IF(cte1.capacity < cte1.waited_passengers_cnt + cte2.diff, cte1.capacity, cte1.waited_passengers_cnt + cte2.diff) AS passengers_cnt
     FROM cte1
     JOIN cte2
     ON cte1.row_num = cte2.row_num + 1
 )
 
-SELECT
-    bus_id,
-    passengers_cnt
+SELECT bus_id, passengers_cnt
 FROM cte2
 ORDER BY bus_id ASC;
