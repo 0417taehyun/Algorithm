bb42c25ce324da6863f557c411e02c5c359ca91d
0417taehyun <0417taehyun@gmail.com>
2022-10-02 16:34:22 +0900

    :recycl: Add simpler solution [ LeetCode ] Python Easy : 278. First Bad Version

--- LeetCode/Python/1_Easy/278_first_bad_version.py
+++ LeetCode/Python/1_Easy/278_first_bad_version.py
@@ -1,6 +1,9 @@
 # [ LeetCode ] 278. First Bad Version
 
 def isBadVersion(version: int) -> bool:
+    """
+    pre-defined API
+    """
     if version >= 4:
         return True
     else:
@@ -19,10 +22,23 @@ def solution(n: int) -> int:
     return answer
 
 
+def another_solution(n: int) -> int:
+    start, end = 1, n
+    while start <= end:
+        middle: int = start + (end - start) // 2
+        if isBadVersion(version=middle):
+            end = middle - 1
+        else:
+            start = middle + 1
+    
+    return start
+    
+
 if __name__ == "__main__":
     case: dict[str, dict[str, int] | int] = {
         "input": {"n": 5},
         "output": 4
     }
-    assert case["output"] == solution(n=case["input"]["n"])
+    assert case["output"] == solution(**case["input"])
+    assert case["output"] == another_solution(**case["input"])
     
