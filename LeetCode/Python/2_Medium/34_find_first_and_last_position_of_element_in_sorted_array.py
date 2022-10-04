69a3cdef3ad3f9f7871d710291a0f87185957f7a
0417taehyun <0417taehyun@gmail.com>
2022-10-04 13:42:47 +0900

    :recycle: Add Sub-Function Solution [ LeetCode ] Python Medium : 34. Find First and Last Position of Element in Sorted Array

--- LeetCode/Python/2_Medium/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ LeetCode/Python/2_Medium/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -42,6 +42,44 @@ def solution(nums: list[int], target: int) -> list[int]:
     return answer
 
 
+def another_soluion(nums: list[int], target: int) -> list[int]:
+    def find_index(nums: list[int], target: int, is_from_left: bool) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            middle: int = start + (end - start) // 2
+            compare: int = nums[middle]
+            if compare > target:
+                end = middle - 1
+            elif compare < target:
+                start = middle + 1
+            else:
+                if is_from_left:
+                    if middle == start or nums[middle - 1] < target:
+                        return middle
+                    else:
+                        end = middle - 1
+            
+                else:
+                    if middle == end or nums[middle + 1] > target:
+                        return middle
+                    else:
+                        start = middle + 1
+                        
+            return -1
+        
+        
+        left_index: int = find_index(
+            nums=nums, target=target, is_from_left=True
+        )
+        if left_index == -1:
+            return [-1, -1]
+        else:
+            right_index: int = find_index(
+                nums=nums, target=target, is_from_left=False
+            )
+            return [left_index, right_index]
+
+
 if __name__ == "__main__":
     cases: list[dict[str, dict[str, list[int] | int] | list[int]]] = [
         {
