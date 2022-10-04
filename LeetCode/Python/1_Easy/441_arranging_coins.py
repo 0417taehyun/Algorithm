105997a12bb55a68ba5c4b2b6a3464021a06a672
0417taehyun <0417taehyun@gmail.com>
2022-10-04 14:25:07 +0900

    :recycle: Add Binary Search and Mathmathical Solution [ LeetCode ] Python Easy : 441. Arranging Coins

--- LeetCode/Python/1_Easy/441_arranging_coins.py
+++ LeetCode/Python/1_Easy/441_arranging_coins.py
@@ -8,6 +8,23 @@ def solution(n: int) -> int:
     return stair - 1
 
 
+def another_solution(n: int) -> int:
+    start, end = 1, n
+    while start <= end:
+        middle: int = start + (end - start) // 2
+        stair: int = (middle * (middle + 1)) // 2
+        if stair <= n:
+            start = middle + 1
+        else:
+            end = middle - 1
+    
+    return end
+
+
+def math_solution(n: int) -> int:
+    return int((2*n+0.25)**0.5-0.5)
+ 
+
 if __name__ == "__main__":
     cases: list[dict[str, dict[str, int] | int]] = [
         { "input": { "n": 5 }, "output": 2 },
