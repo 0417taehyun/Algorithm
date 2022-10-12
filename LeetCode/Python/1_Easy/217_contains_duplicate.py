36fab2a22d78f3f2162dcabcabfdd8ccb413e094
0417taehyun <0417taehyun@gmail.com>
2022-10-13 00:59:38 +0900

    :recycle: Add Hash Map Solution [ LeetCode ] Python Easy : 217. Contains Duplicate

--- LeetCode/Python/1_Easy/217_contains_duplicate.py
+++ LeetCode/Python/1_Easy/217_contains_duplicate.py
@@ -1,14 +1,25 @@
 # [ LeetCode ] 217. Contains Duplicate
 
 def solution(nums: list[int]) -> bool:
-    seen: set = set()
-    for num in nums:
-        if num in seen:
+    numbers: set = set()
+    for number in nums:
+        if number in numbers:
             return True
         else:
-            seen.add(num)
+            numbers.add(number)
     
     return False
+
+
+def another_solution(nums: list[int]) -> bool:
+    numbers: dict[int, bool] = {}
+    for number in nums:
+        if number in numbers:
+            return True
+        else:
+            numbers[number] = True
+    
+    return False    
 
 
 if __name__ == "__main__":
@@ -28,4 +39,5 @@ if __name__ == "__main__":
     ]
     for case in cases:
         assert case["output"] == solution(nums=case["input"]["nums"])
+        assert case["output"] == another_solution(nums=case["input"]["nums"])
     
