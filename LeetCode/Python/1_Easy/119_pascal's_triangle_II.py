41257238a922dab09fbd1267c48b9e9999d9599f
0417taehyun <0417taehyun@gmail.com>
2022-10-16 01:33:47 +0900

    :recycle: Add Pascal's Rule Solution [ LeetCode ] Python Easy : 119. Pascal's Triangle II

--- LeetCode/Python/1_Easy/119_pascal's_triangle_II.py
+++ LeetCode/Python/1_Easy/119_pascal's_triangle_II.py
@@ -12,6 +12,37 @@ def solution(rowIndex: int) -> list[int]:
     return answer
 
 
+def another_solution(rowIndex: int) -> list[int]:
+    answer: list[int] = [1]
+    for row in range(1, rowIndex + 1):
+        for index in range(row - 1, 0, -1):
+            answer[index] = answer[index] + answer[index - 1]
+        
+        answer.append(1)
+    
+    return answer
+    
+
+def math_solution(rowIndex: int) -> list[int]:
+    def combinator(n: int, r: int) -> int:
+        top, bottom = 1, 1
+        for number in range(n, n - r, -1):
+            top *= number
+            
+        for number in range(r, 0, -1):
+            bottom *= number
+        
+        return top // bottom
+
+    answer: list[int] = [
+        combinator(n=rowIndex, r=index)
+        for index in range(rowIndex)
+    ]
+    answer.append(1)
+    
+    return answer
+
+
 if __name__ == "__main__":
     cases: list[dict[str, dict[str, int] | list[int]]] = [
         {
@@ -29,4 +60,6 @@ if __name__ == "__main__":
     ]
     for case in cases:
         assert case["output"] == solution(**case["input"])
+        assert case["output"] == another_solution(**case["input"])
+        assert case["output"] == math_solution(**case["input"])
         
