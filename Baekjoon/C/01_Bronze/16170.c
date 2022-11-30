9b7f4757a3eea4b6ed719a57f4158ff0acc2ea2a
0417taehyun <0417taehyun@gmail.com>
2022-11-30 22:42:53 +0900

    :recycle: Use Struct Pointer [ 백준 ] C Bronze : 16170. 오늘의 날짜는?

--- Baekjoon/C/01_Bronze/16170.c
+++ Baekjoon/C/01_Bronze/16170.c
@@ -6,25 +6,25 @@
 int YEAR_ADDER = 1900;
 int MONTH_ADDER = 1;
 
-int GetYear(struct tm tm)
+int GetYear(struct tm *tm)
 {
-    return tm.tm_year + YEAR_ADDER;
+    return tm -> tm_year + YEAR_ADDER;
 }
 
-int GetMonth(struct tm tm)
+int GetMonth(struct tm *tm)
 {
-    return tm.tm_mon + 1;
+    return tm -> tm_mon + 1;
 }
 
-int GetDay(struct tm tm)
+int GetDay(struct tm *tm)
 {
-    return tm.tm_mday;
+    return tm -> tm_mday;
 }
 
 void PrintCurrentDate()
 {
     time_t now = time(NULL);
-    struct tm tm = *gmtime(&now);
+    struct tm *tm = gmtime(&now);
     printf("%d\n%d\n%d\n", GetYear(tm), GetMonth(tm), GetDay(tm));
 }
 
