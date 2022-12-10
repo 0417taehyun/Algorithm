6d2a1b73c223489ef63202bace95b52d7930e4e0
0417taehyun <0417taehyun@gmail.com>
2022-12-10 21:49:25 +0900

    :recycle: Add Time and Space Optimized Solution by getting maximum and minimum node everytime [ LeetCode ] Python Medium : 1026. Maximum Difference Between Node and Ancestor

--- LeetCode/Python/2_Medium/1026_maximum_difference_between_node_and_ancestor.py
+++ LeetCode/Python/2_Medium/1026_maximum_difference_between_node_and_ancestor.py
@@ -34,3 +34,31 @@ def solution(root: TreeNode) -> int:
     nodes: list[int] = [root.val]
     preorder_traverse(root, nodes)
     return answer
+
+
+def another_solution(root: TreeNode) -> int:
+    def preorder_traverse(
+        node: TreeNode, minimum_node: int, maximum_node: int
+    ) -> None:
+        nonlocal answer
+        if node:
+            if node.val > maximum_node:
+                maximum_node = node.val
+            elif node.val < minimum_node:
+                minimum_node = node.val
+            
+            if not node.left and not node.right:
+                target: int = maximum_node - minimum_node
+                if target > answer:
+                    answer = target
+            if node.left:
+                preorder_traverse(node.left, minimum_node, maximum_node)
+            if node.right:
+                preorder_traverse(node.right, minimum_node, maximum_node)
+    
+    
+    answer: int = 0
+    minimum_node: int = root.val
+    maximum_node: int = root.val
+    preorder_traverse(root, minimum_node, maximum_node)
+    return answer
