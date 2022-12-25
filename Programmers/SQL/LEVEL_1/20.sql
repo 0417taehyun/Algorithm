793e815c3e3fc1679483b44f627bfac9a4108624
taehyun <0417taehyun@gmail.com>
2022-12-25 23:14:28 +0900

    :memo: [ 프로그래머스 ] SQL LEVEL 1 : 조건에 맞는 도서 리스트 출력하기

---
+++ Programmers/SQL/LEVEL_1/20.sql
@@ -0,0 +1,12 @@
+-- [ 프로그래머스 ] 조건에 맞는 도서 리스트 출력하기
+
+SELECT
+    BOOK_ID,
+    DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DAET
+FROM BOOK
+WHERE (
+    YEAR(PUBLISHED_DATE) = 2021
+    AND
+    CATEGORY = '인문'
+)
+ORDER BY PUBLISHED_DATE ASC;
