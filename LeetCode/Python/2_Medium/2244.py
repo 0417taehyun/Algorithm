aa61774b361faf3020d7e837a4ddf778b9ff834c
taehyun <0417taehyun@gmail.com>
2023-01-04 23:09:24 +0900

    :memo: [ LeetCode ] Python Medium : 2244. Minimum Rounds to Complete All Tasks

--- LeetCode/Python/2_Medium/2244.py
+++ LeetCode/Python/2_Medium/2244.py
@@ -1,1 +1,17 @@
 # [ LeetCode ] 2244. Minimum Rounds to Complete All Tasks
+
+def solution(tasks: list[int]) -> int:
+    from collections import defaultdict
+
+
+    answer: int = 0
+    counts: dict[int, int] = defaultdict(int)
+    for difficulty in tasks:
+        counts[difficulty] += 1
+    for difficulty, count in counts.items():
+        if count == 1:
+            return -1
+        answer += (count // 3)
+        if (count % 3) > 0:
+            answer += 1
+    return answer
