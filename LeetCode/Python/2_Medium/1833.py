134605ce2bf31574153a950f20f4c24c8633487e
taehyun <0417taehyun@gmail.com>
2023-01-07 22:09:36 +0900

    :recycle: Add Count Sort Solution [ LeetCode ] Python Medium : 1833. Maximum Ice Cream Bars

--- LeetCode/Python/2_Medium/1833.py
+++ LeetCode/Python/2_Medium/1833.py
@@ -24,3 +24,18 @@ def another_solution(costs: list[int], coins: int) -> int:
         coins -= cost
         answer += 1
     return answer
+
+
+def count_sort_solution(costs: list[int], coins: int) -> int:
+    max_cost: int = max(costs)
+    bucket: list[int] = [0] * (max_cost + 1)
+    for cost in costs:
+        bucket[cost] += 1
+    answer: int = 0
+    for cost in range(1, max_cost+1):
+        if not bucket[cost]:
+            continue
+        count: int = min(bucket[cost], coins//cost)
+        answer += count
+        coins -= (cost * count) 
+    return answer
