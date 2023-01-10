5c740ab8a2cbcdd4da8b967af78f65a0e1ec5997
taehyun <0417taehyun@gmail.com>
2023-01-10 23:28:44 +0900

    :recycle: Add Constant Space Complexity soltuion by reassigning the array element [ LeetCode ] Python Medium : 255. Verify Preorder Sequence in Binary Search Tree

--- LeetCode/Python/2_Medium/255.py
+++ LeetCode/Python/2_Medium/255.py
@@ -10,4 +10,17 @@ def solution(preorder: list[int]) -> bool:
             low = stack.pop()
         stack.append(value)
     return True
-    
+
+
+def another_solution(preorder: list[int]) -> bool:
+    low: int = 0
+    index: int = 0
+    for node in preorder:
+        if node < low:
+            return False
+        while index > 0 and node > preorder[index-1]:
+            low = preorder[index-1]
+            index -= 1
+        preorder[index] = node
+        index += 1
+    return True
