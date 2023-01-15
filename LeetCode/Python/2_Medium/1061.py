70990ebb54da9478622f7ef04e4660198790adc5
taehyun <0417taehyun@gmail.com>
2023-01-15 21:49:45 +0900

    :recycle: Refactoring the root object from integer array to hash map [ LeetCode ] Python Medium : 1061. Lexicographically Smallest Equivalent String

--- LeetCode/Python/2_Medium/1061.py
+++ LeetCode/Python/2_Medium/1061.py
@@ -1,18 +1,21 @@
 # [ LeetCode ] 1061. Lexicographically Smallest Equivalent String
 
 class UnionFind:
-    def __init__(self, size: int) -> None:
-        self.root: list[int] = [ vertex for vertex in range(size) ]
+    def __init__(self) -> None:
+        self.root: dict[str, str] = {
+            chr(alphabet): chr(alphabet)
+            for alphabet in range(ord('a'), ord('z')+1)
+        }
 
-    def find(self, vertex: int) -> int:
-        if vertex == self.root[vertex]:
+    def find(self, vertex: str) -> str:
+        if vertex == self.root.get(vertex):
             return vertex
-        self.root[vertex] = self.find(vertex=self.root[vertex])
-        return self.root[vertex]
+        self.root[vertex] = self.find(vertex=self.root.get(vertex))
+        return self.root.get(vertex)
 
-    def union(self, u: int, v: int) -> None:
-        u_root: int = self.find(vertex=u)
-        v_root: int = self.find(vertex=v)
+    def union(self, u: str, v: str) -> None:
+        u_root: str = self.find(vertex=u)
+        v_root: str = self.find(vertex=v)
         if u_root != v_root:
             if u_root < v_root:
                 self.root[v_root] = u_root
@@ -22,12 +25,10 @@ class UnionFind:
 
 def solution(s1: str, s2: str, baseStr: str) -> str:
     answer: list[str] = []
-    START: int = ord('a')
-    END: int = ord('z')
-    union_find: UnionFind = UnionFind(size=END-START+1)
+    union_find: UnionFind = UnionFind()
     for u, v in zip(s1, s2):
-        union_find.union(u=ord(u)-START, v=ord(v)-START)
+        union_find.union(u=u, v=v)
     for target in baseStr:
-        root: str = chr(union_find.find(vertex=ord(target)-START)+START)
+        root: str = union_find.find(vertex=target)
         answer.append(root)
     return "".join(answer)
